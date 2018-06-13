d20edd5c12a476a5c512b963bfcf7c5a50f48cea
Push <crashjohnson@gmail.com>
2018-06-12 23:37:06 -0500

    designed basic bootstrap page, connected moment.js & firebase. small pseudocode.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
 
+    var config = {
+        apiKey: "AIzaSyDmWkwTbAol6LvYK6DIgRo41m2VG9r99x4",
+        authDomain: "train-scheduler-b006b.firebaseapp.com",
+        databaseURL: "https://train-scheduler-b006b.firebaseio.com",
+        projectId: "train-scheduler-b006b",
+        storageBucket: "",
+        messagingSenderId: "512611692411"
+    };
+
+    firebase.initializeApp(config);
+
+    let database = firebase.database();
+
+    //on submit: take all values and enters them into an object with unique id
+
+    //take values (convert times) and add them to database
+
+    //take most recent database values and append them to DOM
 
 
 
