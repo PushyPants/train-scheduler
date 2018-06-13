2eee4b63e1fe89c200dfe6c3767c7c38ce07250e
Push <crashjohnson@gmail.com>
2018-06-13 01:36:48 -0500

    submit button updates submission count & trains objects in db

--- js/main.js
+++ js/main.js
@@ -12,8 +12,43 @@ $(document).ready(function(){
     firebase.initializeApp(config);
 
     let database = firebase.database();
+    let numSubmits; 
+    
+    //sets the number of submissions variable to the value in the db
+    database.ref('/submits').on('value', function(snapshot){
+        numSubmits = snapshot.val();
+    });
+    
+    
+
+    //Working code to set database information
+        // database.ref('/trains').set({
+        //     trainId:{
+        //         trainName : 'Name 1',
+        //         dest : 'Dest 1',
+        //         freq : 'Freq 1',
+        //         nextArrival : 'Arrival 1',
+        //     }
+        // })
 
     //on submit: take all values and enters them into an object with unique id
+    $('#trainSubmit').on('click', function(event){
+        event.preventDefault();
+        numSubmits++
+        database.ref('/submits').set(numSubmits);
+        console.log(numSubmits)
+
+        
+        database.ref('/trains').update({
+                ['trainId-'+numSubmits] :{
+                    trainName : $('#train-name').val(),
+                    dest : $('#destination').val(),
+                    freq : $('#train-time').val(),
+                    nextArrival : $('#frequency').val(),
+                }
+            })
+
+    });
 
     //take values (convert times) and add them to database
 
